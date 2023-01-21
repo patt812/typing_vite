618b223b45630cb49f752d8a26b9e3f4d82903c6
patt812 <dev.patt812@gmail.com>
2023-01-21 15:03:03 +0900

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'App\Verify\Service',
+            'App\Services\Twilio\Verification'
+        );
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        URL::forceScheme('https');
+    }
+}
