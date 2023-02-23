39e21ef489020fe4e797bb789967cddfd23adef1
patt812 <dev.patt812@gmail.com>
2023-02-23 14:22:07 +0900

    #22 ラジオボタンのスタイルを設定

---
+++ resources/js/Components/RadioInput.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+import { onMounted, ref } from 'vue';
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        default: ''
+    },
+    checked: {
+        type: [Array, Boolean],
+        default: false
+    }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+
+const proxyChecked = computed({
+    get() {
+        return props.checked;
+    },
+
+    set(val) {
+        emit('update:checked', val);
+    },
+});
+</script>
+
+<template>
+    <input ref="input" type="radio" v-model="proxyChecked"
+        class="bg-main border-2 border-black border-dashed checked:bg-black focus:ring-black focus:checked:bg-black focus:opacity-70 focus:ring-opacity-50 active:checked:ring-black active:checked:bg-black hover:checked:bg-black hover:ring-black hover:opacity-60 shadow-sm"
+        :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+</template>
