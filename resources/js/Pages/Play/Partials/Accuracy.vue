35bd68c8116c4eae90e139b18ed6368764c09378
patt812 <dev.patt812@gmail.com>
2023-03-05 18:41:00 +0900

    #22 測定デザイン実装

---
+++ resources/js/Pages/Play/Partials/Accuracy.vue
@@ -0,0 +1,138 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+
+const props = defineProps({
+    accuracy: {
+        type: Number,
+        default: 0,
+    },
+});
+
+const offset = computed(() => {
+    if (props.accuracy <= 0) {
+        return 251;
+    } else if (props.accuracy >= 100) {
+        return 0;
+    } else {
+        return (100 - props.accuracy) * 251 / 100;
+    }
+})
+
+</script>
+
+<template>
+    <div class="box select-none" :style="`stroke-dashoffset: ${offset};`">
+        <div class="percent" :style="`stroke-dashoffset: ${offset};`">
+            <svg>
+                <circle class="base" cx="45" cy="45" r="40"></circle>
+                <circle class="line" cx="45" cy="45" r="40" :style="`stroke-dashoffset: ${offset};`"></circle>
+            </svg>
+            <div class="number">
+                <div class="title">{{ accuracy }}<span>%</span></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.box {
+    position: relative;
+    min-width: 100px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+}
+
+.box .percent svg {
+    position: relative;
+    width: 90px;
+    height: 90px;
+    -webkit-transform: rotate(-90deg);
+    transform: rotate(-90deg);
+}
+
+.box .percent svg circle {
+    position: relative;
+    fill: none;
+    stroke-width: 7;
+    stroke: #f3f3f3;
+    stroke-dasharray: 251;
+    stroke-dashoffset: 0;
+    stroke-linecap: round;
+}
+
+.box .percent .number {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    color: #111;
+}
+
+.box .percent .number .title {
+    font-size: 1.8rem;
+}
+
+.box .percent .number .title span {
+    font-size: 1rem;
+}
+
+.box .text {
+    padding: 10px 0 0;
+    text-align: center;
+    font-weight: bold;
+    font-size: 14px;
+}
+
+.box .percent .line {
+    -webkit-animation: circleAnim 1s forwards;
+    animation: circleAnim 1s forwards;
+}
+
+.box .percent .line {
+    stroke-dashoffset: 0;
+    stroke: #000;
+}
+
+@-webkit-keyframes circleAnim {
+    0% {
+        stroke-dasharray: 0 251;
+    }
+
+    99.9%,
+    to {
+        stroke-dasharray: 251 251;
+    }
+}
+
+@keyframes circleAnim {
+    0% {
+        stroke-dasharray: 0 251;
+    }
+
+    99.9%,
+    to {
+        stroke-dasharray: 251 251;
+    }
+}
+</style>
---
+++ resources/js/Pages/Play/Partials/SpeedMeter.vue
@@ -0,0 +1,127 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+
+const props = defineProps({
+    angle: {
+        type: Number,
+        default: 0,
+    },
+});
+
+const clampedAngle = computed(() => {
+    if (Number.isNaN(props.angle)) return - 90;
+    const angle = (props.angle / 600 * 90) - 90;
+    return Math.min(angle, 0);
+});
+</script>
+
+<template>
+    <div class="wrap">
+        <div class="speed-meter">
+            <div class="circle">
+                <div class="needle" :style="`transform: rotate(${clampedAngle}deg)`"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.wrap {
+    border: 3px solid #000;
+    height: 90px;
+    width: 200px;
+    overflow: hidden;
+}
+
+.speed-meter {
+    transform: rotate(42deg);
+    position: relative;
+    left: 20px;
+}
+
+.circle {
+    position: relative;
+    width: 140px;
+    height: 130px;
+    border-radius: 100% 0 0 0;
+    border: 3px solid black;
+    border-bottom: 0;
+    border-right: 0;
+}
+
+.needle {
+    background: black;
+    height: 7px;
+    right: 13px;
+    position: absolute;
+    bottom: 5px;
+    width: 0;
+    transform-origin: 50% 100%;
+    margin: -105px -4px 0;
+    padding: 105px 4px 0;
+    z-index: 2;
+    border-radius: 50% 50% 0 0;
+    transform: rotate(-90deg);
+
+    /* animation: pendulum; */
+    animation-iteration-count: infinite;
+    animation-duration: 4s;
+
+}
+
+#speed-meter {
+    background: #171717;
+    margin: 100px auto;
+    width: 600px;
+    height: 600px;
+    box-shadow: inset 0px 0px 9px 3px rgba(0, 0, 0, 1);
+    border: 5px solid #171717;
+    border-radius: 100%;
+    display: block;
+    position: relative;
+}
+
+
+@keyframes pendulum {
+    10% {
+        transform: rotate(0deg);
+    }
+
+
+    11% {
+        transform: rotate(-2deg);
+    }
+
+    12% {
+        transform: rotate(-4deg);
+    }
+
+    13% {
+        transform: rotate(-2deg);
+    }
+
+    14% {
+        transform: rotate(-4deg);
+    }
+
+    15% {
+        transform: rotate(-2deg);
+    }
+
+    16% {
+        transform: rotate(-4deg);
+    }
+
+    17% {
+        transform: rotate(-2deg);
+    }
+
+    18% {
+        transform: rotate(-4deg);
+    }
+
+    19% {
+        transform: rotate(-2deg);
+    }
+}
+</style>
