acc59cfdd8cd013550d3657985b43ba44fbd79e3
patt812 <dev.patt812@gmail.com>
2023-03-19 20:50:46 +0900

    #22 認証カードにサイト名を追加

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -43,6 +43,7 @@ class HandleInertiaRequests extends Middleware
         }
 
         return array_merge(parent::share($request), [
+            'app_name' => env('APP_NAME', 'Laravel'),
             'user' => $user,
             'flash' => [
                 'message' => fn () => $request->session()->get('message')
