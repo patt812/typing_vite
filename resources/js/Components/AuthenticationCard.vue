a20cad2dbcd4dfde95c08a3b9aa1497660185a96
patt812 <dev.patt812@gmail.com>
2023-03-28 21:37:11 +0900

    #2 認証のタイトルリボンにリンクを設置

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -4,9 +4,11 @@ import ContentFrame from '@/Components/ContentFrame.vue';
 
 <template>
     <div class="bg-main min-h-screen max-w-[2560px] mx-auto relative mt-[-10px] flex flex-col justify-center items-center">
-        <div class="absolute z-10 top-10 -left-10 bg-black text-white px-16 py-1 -rotate-45 font-bold">
-            {{ $page.props.app_name }}
-        </div>
+        <a href="/">
+            <div class="select-none absolute z-10 top-10 -left-10 bg-black text-white px-16 py-1 -rotate-45 font-bold">
+                {{ $page.props.app_name }}
+            </div>
+        </a>
         <div>
             <slot name="logo" />
         </div>
